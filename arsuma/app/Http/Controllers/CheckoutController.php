7a1de58003bc5b0ade617f8bb589db2e8c4468ba
Randy AP <rrandy.kun@gmail.com>
2021-02-28 21:41:13 +0700

    menambahkan route checkout

--- arsuma/app/Http/Controllers/CheckoutController.php
+++ arsuma/app/Http/Controllers/CheckoutController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class CheckoutController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        return view('pages.checkout');
+    }
 }

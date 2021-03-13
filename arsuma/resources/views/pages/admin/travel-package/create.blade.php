444b0c65901e05726369543a6437695cdca05516
Randy AP <rrandy.kun@gmail.com>
2021-03-14 01:03:04 +0700

    menambahkan fungsi CRUD baca data paket travel

---
+++ arsuma/resources/views/pages/admin/travel-package/create.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Tambah Paket Travel</h1>
+    </div>
+
+    <div class="row">
+        <div class="card-body">
+            
+        </div>
+    </div>
+
+
+</div>
+<!-- /.container-fluid -->
+
+@endsection

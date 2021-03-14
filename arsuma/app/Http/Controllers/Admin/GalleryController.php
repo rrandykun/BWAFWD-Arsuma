1542364a983da4d419e84f070d27f9107baec075
Randy AP <rrandy.kun@gmail.com>
2021-03-14 10:35:05 +0700

    menambahkan fungsi CRUD, update data gallery

--- arsuma/app/Http/Controllers/Admin/GalleryController.php
+++ arsuma/app/Http/Controllers/Admin/GalleryController.php
@@ -74,8 +74,12 @@ class GalleryController extends Controller
     public function edit($id)
     {
         $item = Gallery::findOrFail($id);
+        $travel_packages = TravelPackage::all();
 
-        return view('pages.admin.gallery.edit', ['item' => $item]);
+        return view('pages.admin.gallery.edit', [
+            'item' => $item,
+            'travel_packages' => $travel_packages
+        ]);
     }
 
     /**
@@ -88,7 +92,10 @@ class GalleryController extends Controller
     public function update(GalleryRequest $request, $id)
     {
         $data = $request->all();
-        $data['slug'] = Str::slug($request->title);
+        $data['image'] = $request->file('image')->store(
+            'assets/gallery',
+            'public'
+        );
 
         $item = Gallery::findOrFail($id);
 

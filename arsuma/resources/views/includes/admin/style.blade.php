d84fd89def6b08e3d786c36ad9cbfd7ed5994726
Randy AP <rrandy.kun@gmail.com>
2021-03-17 20:36:02 +0700

    menambahkan favicon

--- arsuma/resources/views/includes/admin/style.blade.php
+++ arsuma/resources/views/includes/admin/style.blade.php
@@ -1,3 +1,9 @@
+{{-- favicon --}}
+{{-- <link rel="icon" href="{{ url('favicon.ico') }}" type="image/x-icon"/> --}}
+<link rel="apple-touch-icon" sizes="180x180" href="{{ url('favicon_io/apple-touch-icon.png')}}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ url('favicon_io/favicon-32x32.png')}}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ url('favicon_io/favicon-16x16.png')}}">
+<link rel="manifest" href="{{ url('favicon_io/site.webmanifest')}}">
 <!-- Custom fonts for this template-->
 <link href="{{ url('backend/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
 <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
--- arsuma/resources/views/includes/style.blade.php
+++ arsuma/resources/views/includes/style.blade.php
@@ -1,3 +1,10 @@
+{{-- favicon --}}
+{{-- <link rel="icon" href="{{ url('favicon.ico') }}" type="image/x-icon"/> --}}
+<link rel="apple-touch-icon" sizes="180x180" href="{{ url('/favicon_io/apple-touch-icon.png')}}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ url('/favicon_io/favicon-32x32.png')}}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ url('/favicon_io/favicon-16x16.png')}}">
+<link rel="manifest" href="{{ url('/favicon_io/site.webmanifest')}}">
+
 <link rel="stylesheet" href="{{ url('frontend/libraries/bootstrap/css/bootstrap.css')}}" />
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@200;300;400;500;600;700;800&family=Playfair+Display:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
